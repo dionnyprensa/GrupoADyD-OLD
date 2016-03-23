b02379d074ea4d6d7f8a1431a6829b2003d7e9d1
diio <d.prensa1@gmail.com>
2016-03-23 17:43:34 -0400

    Se esta trabajando en la insercion de productos en la tabla, y el manejo de la persistencia, ya que los datos son cargados via Ajax.

--- GrupoADyD/Controllers/SalesController.cs
+++ GrupoADyD/Controllers/SalesController.cs
@@ -1,5 +1,6 @@
 ﻿using GrupoADyD.Models;
 using GrupoADyD.Models.Repositories;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
@@ -12,6 +13,15 @@ namespace GrupoADyD.Controllers
     {
         private ClientRepository ClientRepository = new ClientRepository();
         private ApplicationDbContext db = new ApplicationDbContext();
+        private ICollection<Product> ProductsTable = new List<Product>();
+        private ICollection<Product> Duplicates = new List<Product>();
+        private decimal TotalPV = 0;
+        private decimal TotalBV = 0;
+        private decimal TotalCost = 0;
+        private decimal TotalPrice = 0;
+        private decimal Total = 0;
+        private short IndexProduct = 1;
+        private decimal TotalPerProduct = 0;
 
         // GET: Sales
         public async Task<ActionResult> Index()
@@ -39,6 +49,7 @@ namespace GrupoADyD.Controllers
         public ActionResult Create()
         {
             ViewBag.Clients = new SelectList(db.Clients, "ClientId", "FirstName");
+
             //ViewBag.Products = new SelectList(db.Products, "ProductId", "Name");
             return View();
         }
@@ -50,16 +61,27 @@ namespace GrupoADyD.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "SaleId,Total,RowVersion,CreationDate,ModificationDate")] Sale sale)
         {
+            ViewBag.SaleId = new SelectList(db.Clients, "ClientId", "FirstName"
+                , sale.SaleId);
+
             if (ModelState.IsValid)
             {
                 db.Sales.Add(sale);
                 await db.SaveChangesAsync();
+
+                TotalPV = 0;
+                TotalBV = 0;
+                TotalCost = 0;
+                TotalPrice = 0;
+                Total = 0;
+
                 return RedirectToAction("Index");
             }
-
-            ViewBag.SaleId = new SelectList(db.Clients, "ClientId", "FirstName"
-                , sale.SaleId);
-            return View(sale);
+            else
+            {
+                ModelState.AddModelError(null, "Error");
+                return View(sale);
+            }
         }
 
         // GET: Sales/Edit/5
@@ -81,8 +103,7 @@ namespace GrupoADyD.Controllers
         }
 
         // POST: Sales/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "SaleId,Total,RowVersion,CreationDate,ModificationDate")] Sale sale)
@@ -142,7 +163,6 @@ namespace GrupoADyD.Controllers
 
         public ActionResult FindProduct(string NameOrCode)
         {
-
             var productName = db.Products.FirstOrDefault(p => p.Name.Contains(NameOrCode));
 
             if (productName == null)
@@ -160,7 +180,71 @@ namespace GrupoADyD.Controllers
         {
             var product = db.Products.FirstOrDefault(p => p.Code == Code);
 
-            return PartialView("_TableSale", product);
+            ViewBag.HasProduct = (product.Code.ToString().Length > 1) ? true : false;
+
+            if (ViewBag.HasProduct)
+            {
+                if (TempData["ProductsTable"] != null)
+                {
+                    ProductsTable = TempData["ProductsTable"] as List<Product>;
+
+                    Total = Total + 1;
+                }
+
+                ProductsTable.Add(product);
+
+                foreach (var e in Duplicates)
+                {
+                    var a = e.Code.ToString();
+                }
+
+                /*
+                List<String> duplicates = lst.GroupBy(x => x)
+                             .Where(g => g.Count() > 1)
+                             .Select(g => g.Key)
+                             .ToList();
+                */
+
+                TotalPV = product.PV;
+                TotalBV = product.BV;
+                TotalCost = product.Cost;
+                TotalPrice = product.Price;
+
+                //Total = product.Price;
+
+                DataPersist();
+            }
+
+            return PartialView("_TableSale", ProductsTable);
+        }
+
+        void DataPersist()
+        {
+            if(TempData["TotalPV"] != null)
+            {
+                var tpv = TempData["TotalPV"];
+                var tbv = TempData["TotalBV"];
+                var tc = TempData["TotalCost"];
+                var tp = TempData["TotalPrice"];
+                var t = TempData["Total"];
+                var tpr = TempData["TotalPerProduct"];
+
+                TotalPV += (decimal)tpv;
+                TotalBV += (decimal)tbv;
+                TotalCost += (decimal)tc;
+                TotalPrice += (decimal)tp;
+                Total += (decimal)t;
+                TotalPerProduct += (decimal)tpr;
+            }
+
+            TempData["TotalPV"] = TotalPV;
+            TempData["TotalBV"] = TotalBV;
+            TempData["TotalCost"] = TotalCost;
+            TempData["TotalPrice"] = TotalPrice;
+            TempData["Total"] = Total;
+            TempData["TotalPerProduct"] = TotalPerProduct;
+            TempData["IndexProduct"] = 1;
+            TempData["ProductsTable"] = ProductsTable;
         }
     }
 }
